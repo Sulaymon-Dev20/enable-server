f82dffd87aa9ae71029ea12c9ce5c3ba7d8382aa
Sulaymon Yahyo 🪐 <sulaymon1w@gmail.com>
2023-08-15 19:54:42 +0500

    🧱 First Commit

---
+++ src/main/java/com/example/evrika/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.evrika.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final SecurityUser securityUser;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername(securityUser.getName())
+            .password(passwordEncoder.encode(securityUser.getPassword()))
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf()
+            .disable()
+            .authorizeHttpRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/example/evrika/config/SecurityUser.java
@@ -0,0 +1,13 @@
+package com.example.evrika.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties(prefix = "spring.security.user")
+@Configuration("user")
+@Data
+public class SecurityUser {
+    private String name;
+    private String password;
+}
