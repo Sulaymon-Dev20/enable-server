f24387612ba9151e7505bbfa1a8da945c56ecf89
Sulaymon Yahyo 🪐 <sulaymon1w@gmail.com>
2023-08-31 02:08:36 +0500

    🛸 Zipkin integrate done

--- src/main/java/com/example/eureka/config/SecurityConfig.java
+++ src/main/java/com/example/eureka/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.example.eureka.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -30,13 +32,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf()
-            .disable()
-            .authorizeHttpRequests()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .httpBasic();
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/eureka/**", "/eureka").permitAll()
+                .anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
